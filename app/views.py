8f9f6987ea073045b76a735d547e96558b25f93b
christopher <greegmon13@gmail.com>
2025-01-20 10:34:19 +0800

    Add paster function

--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+import requests
+from bs4 import BeautifulSoup
 from flask import (
   Blueprint,
   render_template
@@ -8,3 +10,15 @@ view = Blueprint('view',__name__)
 @view.route('/')
 def root():
   return render_template('home.html', title="Update BOT"),200
+
+@view.route('/paster/<path>')
+def paster(path):
+  res = requests.get(f"https://pastebin.mozilla.org/{path}/raw")
+  soup = BeautifulSoup(res.content, 'html.parser')
+  val = res.text
+  try:
+    if soup.find('title').text == "404 Snippet not found":
+      val = "Paste not found"
+  except Exception as e:
+    pass
+  return render_template('paster.html', text=val, title=f"/paster/{path}"),200
--- util.py
+++ util.py
@@ -104,3 +104,45 @@ def getName(uid):
   except Exception as e:
     print(e)
     return 'Facebook User'
+
+# pase api
+def paster(paste: str, exp: int|None = None) -> dict:
+  try:
+    session = requests.Session()
+    url = "https://pastebin.mozilla.org";
+    
+    headers = {
+      "Content-Type": 'application/x-www-form-urlencoded',
+      "User-Agent": userAgent,
+      "Referer": url
+    }
+    data = {}
+    
+    res = session.get(url).content
+    soup = BeautifulSoup(res, 'html.parser')
+    form = soup.find('form')
+    token = form.find('input', attrs={"name":"csrfmiddlewaretoken"}).get('value')
+    
+    data["csrfmiddlewaretoken"] = token
+    data["title"] = ''
+    data["lexer"] = '_text'
+    data["expires"] = 86400*exp
+    data["content"] = paste
+    
+    response = session.post(url, data=data, headers=headers)
+    resHtml = BeautifulSoup(response.content, 'html.parser')
+    
+    ul = resHtml.find('ul',id="snippetOptions")
+    resTitle = resHtml.find('title').getText()
+    
+    expire = ul.find('li',class_='option-type').getText().split('in: ')[1].strip()
+    path = resTitle.split('Pastebin')[1].split(' ')[0]
+    text = ul.find("textarea", id="copySnippetSource").getText()
+    return {
+      "path": '/paster' + path,
+      "text": text,
+      "expire": ' '.join(expire.split('\xa0'))
+    }
+  except Exception as e:
+    print("Exception: ", e)
+    return {"error": f"{e}"}
